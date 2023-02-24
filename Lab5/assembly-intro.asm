d8268aea048360aa4b2e1821963af646aaee5b66
Autumn Bertram <112494911+xxaleebxx@users.noreply.github.com>
2023-02-24 15:11:01 -0500

    Update assembly-intro.asm

--- Lab5/assembly-intro.asm
+++ Lab5/assembly-intro.asm
@@ -29,7 +29,22 @@ syscall			# It looks in $v0 to determine what to do
 # Lines below here are where you will write your functions.
 #
 monkeyTrouble:
-	jr $ra
+	# Two arguments: a_smile ($a0), b_smile ($a1)
+	# Return: 1 if in trouble, 0 if not in trouble
+	# t0 = a_smile and b_smile
+	add $t0, $a0, $a1
+	# t1 = !a_smile
+	not $t1, $a0
+	andi $t1, $t1, 1
+	# t2 = !b_smile
+	not $t2, $a1
+	andi $t2, $t2, 1
+	# t3 = !a_smile and !b_smile
+	and $t3, $t1, $t2
+	# v0 = t0 or t3
+	or $v0, $t0, $t3
+	
+	jr $ra 
 
 sleepIn:
 	jr $ra
