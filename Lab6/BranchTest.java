3fc8e7ee7cd11b12e92ab416f62759a18376dbf2
thomas <jonestho@mail.gvsu.edu>
2023-03-01 18:55:52 -0500

    complete makes10

--- Lab6/BranchTest.java
+++ Lab6/BranchTest.java
@@ -1,6 +1,9 @@
 import org.junit.*;
 
 import static edu.gvsu.mipsunit.munit.MUnit.Register.*;
+
+import java.beans.Transient;
+
 import static edu.gvsu.mipsunit.munit.MUnit.*;
 
 public class BranchTest {
@@ -14,53 +17,71 @@ public class BranchTest {
 
     @Test 
     public void makes10_aIs10() {
-	run("makes10", 10, 3);
-	Assert.assertEquals(1, get(v0));
+	    run("makes10", 10, 3);
+	    Assert.assertEquals(1, get(v0));
     }
 
-    // Write more tests!
-
-
-    /******************************************************************
-     *
-     * Test intMax
-     *
-     *****************************************************************/
-
-    @Test 
-    public void intMax_ascending() {
-	run("intMax", 5, 6, 7);
-	Assert.assertEquals(7, get(v0));
+    @Test
+    public void makes10_bIs10(){
+        run("makes10", 5, 10);
+        Assert.assertEquals(1, get(v0));
     }
 
-    // Write more tests!
-
-    /******************************************************************
-     *
-     * Test close10
-     *
-     *****************************************************************/
-
-    @Test 
-    public void close10_aClosest() {
-	run("close10", 8, 13);
-	Assert.assertEquals(8, get(v0));
+    @Test
+    public void makes10_sumIs10(){
+        run("makes10", 5, 5);
+        Assert.assertEquals(1, get(v0));
     }
 
-    // Write more tests!
-
-
-    /******************************************************************
-     *
-     * Test dateFashion
-     *
-     *****************************************************************/
-
-    @Test 
-    public void dateFashion_youOk_dateStylish() {
-	run("dateFashion", 5, 10);
-	Assert.assertEquals(2, get(v0));
+    @Test
+    public void makes10_sumNot10(){
+        run("makes10", 3, 6);
+        Assert.assertEquals(0, get(v0));
     }
+
+    // // Write more tests!
+
+
+    // /******************************************************************
+    //  *
+    //  * Test intMax
+    //  *
+    //  *****************************************************************/
+
+    // @Test 
+    // public void intMax_ascending() {
+	// run("intMax", 5, 6, 7);
+	// Assert.assertEquals(7, get(v0));
+    // }
+
+    // // Write more tests!
+
+    // /******************************************************************
+    //  *
+    //  * Test close10
+    //  *
+    //  *****************************************************************/
+
+    // @Test 
+    // public void close10_aClosest() {
+	// run("close10", 8, 13);
+	// Assert.assertEquals(8, get(v0));
+    // }
+
+    // // Write more tests!
+
+
+    // /******************************************************************
+    //  *
+    //  * Test dateFashion
+    //  *
+    //  *****************************************************************/
+
+    // @Test 
+    // public void dateFashion_youOk_dateStylish() {
+	// run("dateFashion", 5, 10);
+	// Assert.assertEquals(2, get(v0));
+    // }
 
     // Write more tests!
 
--- Lab6/branches-intro.asm
+++ Lab6/branches-intro.asm
@@ -18,7 +18,36 @@ addi $a1, $0, 1
 jal dateFashion
 
 
-add $v0, $0, 10
+addi $v0, $0, 10
 syscall
 
+makes10:
+	# Two arguments: a ($a0), b ($a1)
+	# Return: 1 if 10, 0 if not 10
+	
+	addi $t0, $0, 0 # False ($t0)
+	addi $t1, $0, 1 # True ($t1)
+	addi $a2, $0, 10 # $a2 = 10
+	
+	beq $a0, $a2, is_true_10
+	add $a3, $a0, $a1 # $a3 = a + b
+	beq $a3, $a2, is_true_10
+	j end_block
+	
+	beq $a1, $a2, is_true_10
+	add $a3, $a0, $a1 # $a3 = a + b
+	beq $a3, $a2, is_true_10
+	j end_block
+	
+	
+	is_true_10:
+		addi $v0, $0, 1
+		
+	end_block:
+		jr $ra
+		
+intMax:
 
+close10:
+
+dateFashion:
