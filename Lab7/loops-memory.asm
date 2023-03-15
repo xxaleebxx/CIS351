8e5c797291a48e249a742de149758ebb7a49f7a3
Autumn Bertram <112494911+xxaleebxx@users.noreply.github.com>
2023-03-15 17:59:08 -0400

    Add files via upload

---
+++ Lab7/loops-memory.asm
@@ -0,0 +1,41 @@
+.globl indexOf max sum13 sum67
+.text
+
+#addi $a0, $0, 1
+#addi $a1, $0, 1
+#jal indexOf
+
+#addi $a0, $0, 1
+#ddi $a1, $0, 1
+#addi $a2, $0, 1
+#jal max
+
+#addi $a0, $0, 1
+#addi $a1, $0, 1
+#jal sum13
+
+addi $a0, $0, 3
+addi $a1, $0, 3
+jal sum67
+
+
+addi $v0, $0, 10
+syscall
+
+
+indexOf:
+
+
+
+max:
+
+
+
+sum13:
+
+
+
+sum67:
+
+
+
