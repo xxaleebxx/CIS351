52edfbed258ce88963108c17a1e0954b43e5f23e
Autumn Bertram <112494911+xxaleebxx@users.noreply.github.com>
2023-03-22 19:19:11 -0400

    update recursion

--- Lab8/log_two.asm
+++ Lab8/log_two.asm
@@ -1,5 +1,5 @@
-// Thomas Jones
-// Autumn Bertram
+# Thomas Jones
+# Autumn Bertram
 
 
 .globl log_two
@@ -14,34 +14,31 @@ addi $v0, $0, 10
 syscall
 
 log_two:
-    //code here
-    # handle base cases
-    recurr:
-        #space on the stack
-        addi $sp, $sp, -4
-        sw $ra, 0($sp)
+    #code here
+    #space on the stack
+    addi $sp, $sp, -8
+    sw $ra, 0($sp)
+    sw $s0, 0($sp) #sum register
 
-        # basecases
-        addi $t0, $0, $1
-        beq $a0, $t0, base_case_1
+    # basecases
+    addi $t0, $0, 1
+    beq $a0, $t0, base_case_1
 
-        slt $t1, $a0, $0
-        beq $t1, $t0, base_case_neg
+    slt $t1, $a0, $t0
+    beq $t1, $t0, base_case_neg
     
+    # recursion
+    lw $s0, 0($sp)
+    addi $s0, $s0, 1
+    sw $s0, 0($sp)
     
-    
-    
-    
-    
-    
-
+    sra $a0, $a0, 1
+    jal log_two 
 
     base_case_1:
         addi $v0, $0, 0
         jr $ra
 
-
     base_case_neg:
         addi $v0, $0, -1
         jr $ra
-
