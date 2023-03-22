df785a21675868e1dbed77f52dd2fe6bc58074e9
thomas <jonestho@mail.gvsu.edu>
2023-03-22 19:18:30 -0400

    writes some tests

--- Lab8/LogTwoTest.java
+++ Lab8/LogTwoTest.java
@@ -1,6 +1,9 @@
 import org.junit.*;
 
 import static edu.gvsu.mipsunit.munit.MUnit.Register.*;
+
+import java.beans.Transient;
+
 import static edu.gvsu.mipsunit.munit.MUnit.*;
 
 public class LogTwoTest {
@@ -18,16 +21,39 @@ public class LogTwoTest {
 
     @Test 
     public void log_two_one() {
-    run("log_two", 1);
-    Assert.assertEquals(0, get(v0));
+        run("log_two", 1);
+        Assert.assertEquals(0, get(v0));
     }
 
     @Test 
     public void log_two_zero() {
-    run("log_two", 0);
-    Assert.assertEquals(-1, get(v0));
+        run("log_two", 0);
+        Assert.assertEquals(-1, get(v0));
     }
 
+    @Test
+    public void log_two_10() {
+        run("log_two", 10);
+        Assert.assertEquals(3, get(v0));
+    }
+
+    @Test
+    public void log_two_32() {
+        run("log_two", 32);
+        Assert.assertEquals(5, get(v0));
+    }
+
+    @Test
+    public void log_two_100() {
+        run("log_two", 100);
+        Assert.assertEquals(6, get(v0));
+    }
+
+    @Test
+    public void log_two_1024() {
+        run("log_two", 1024);
+        Assert.assertEquals(10, get(v0));
+    }
     /******************************************************************
      *
      * Write many more tests!  * Test Edge Cases *
